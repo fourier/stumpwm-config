049e5fe3d2addb21ae3ffa224b6a5ae49ed2f874
Alexey Veretennikov <fourier@protonmail.ch>
2021-01-15 18:57:58 +0100

    Updated.

--- init.lisp
+++ init.lisp
@@ -4,13 +4,15 @@
 
 (in-package :stumpwm)
 
-;; change the prefix key to something else
-(set-prefix-key (kbd "s-u"))
-;;(run-shell-command "setxkbmap -layout dvorak")
-;; set keyboard layout
-(run-shell-command "setxkbmap -layout dvorak,ru -option \"grp:alt_shift_toggle,caps:ctrl_modifier\"")
+;; load modules
+(load-module "battery-portable")
+(load-module "amixer")
 
-;; Set the modeline
+;; change the prefix key to something else
+(set-prefix-key (kbd "s-t"))
+
+;; Set the modeline and its format
+(setf *screen-mode-line-format* "^BBat %B^b ^B%d^b [^B%n^b] %W ")
 (toggle-mode-line (current-screen) (current-head))
 
 ;; Set "focus follow mouse" focus policy
@@ -18,6 +20,7 @@
 
 ;; Suppress frame indicator
 (setf *suppress-frame-indicator* t)
+
 
 ;; prompt the user for an interactive command. The first arg is an
 ;; optional initial contents.
@@ -29,12 +32,25 @@
 
 (defcommand single-screen () (:rest)
   "Switch to single display setup"
-  (run-shell-command "xrandr --output DP-0 --off"))
+  (run-shell-command "xrandr --output DP-0 --off")
+  (message "Single screen"))
 
 (defcommand dual-screen () (:rest)
   "Switch to dual display setup"
-  (run-shell-command "xrandr --output LVDS-0 --left-of DP-0 --output DP-0 --auto"))
+  (run-shell-command "xrandr --output LVDS-0 --left-of DP-0 --output DP-0 --auto")
+  (message "Dual screen"))
 
+(defcommand urxvt () (:rest)
+  "Switch to URxvt" 
+  (run-or-raise "urxvt" '(:class "URxvt")))
+
+(defcommand capture-screen() (:rest)
+  "Screenshot"
+  (run-shell-command "scrot $HOME/Pictures/Screenshots/screen_%Y-%m-%d-%H-%M-%S.png -d 1"))
+
+(defcommand capture-window() (:rest)
+  "Screenshot of the current window"
+  (run-shell-command "scrot $HOME/Pictures/Screenshots/window_%Y-%m-%d-%H-%M-%S.png -d 1-u"))
 
 ;; to undefine keys use undefine-key 
 
@@ -52,6 +68,18 @@
 (define-key *top-map* (stumpwm:kbd "s-7") "gselect 7")
 (define-key *top-map* (stumpwm:kbd "s-8") "gselect 8")
 (define-key *top-map* (stumpwm:kbd "s-9") "gselect 9")
+;; Move to group
+(define-key *top-map* (stumpwm:kbd "s-)") "gmove 0")
+(define-key *top-map* (stumpwm:kbd "s-!") "gmove 1")
+(define-key *top-map* (stumpwm:kbd "s-@") "gmove 2")
+(define-key *top-map* (stumpwm:kbd "s-#") "gmove 3")
+(define-key *top-map* (stumpwm:kbd "s-$") "gmove 4")
+(define-key *top-map* (stumpwm:kbd "s-%") "gmove 5")
+(define-key *top-map* (stumpwm:kbd "s-^") "gmove 6")
+(define-key *top-map* (stumpwm:kbd "s-&") "gmove 7")
+(define-key *top-map* (stumpwm:kbd "s-*") "gmove 8")
+(define-key *top-map* (stumpwm:kbd "s-(") "gmove 9")
+
 
 ;; Configuration
 (define-key *top-map* (stumpwm:kbd "s-F1") "single-screen")
@@ -74,12 +102,22 @@
 (define-key *top-map* (kbd "s-S-Left")  "move-window left")
 (define-key *top-map* (kbd "s-S-Right") "move-window right")
 
-;; some applications bindings
-(define-key *top-map* (stumpwm:kbd "s-e") "emacs")
-(define-key *top-map* (stumpwm:kbd "s-S-Return") "exec urxvt")
-(define-key *top-map* (stumpwm:kbd "s-p") "exec dmenu_run")
-(define-key *top-map* (stumpwm:kbd "s-S-p") "exec dmenu_run")
+;; Audio controls (based on amixer module)
+(define-key *top-map* (kbd "XF86AudioLowerVolume") "amixer-master-1-")
+(define-key *top-map* (kbd "XF86AudioRaiseVolume") "amixer-master-1+")
+(define-key *top-map* (kbd "XF86AudioMute") "amixer-master-toggle")
 
+;; some applications bindings
+(define-key *top-map* (kbd "s-e") "emacs")
+(define-key *top-map* (kbd "s-S-Return") "exec urxvt")
+(define-key *top-map* (kbd "s-p") "exec dmenu_run")
+(define-key *top-map* (kbd "s-S-p") "exec dmenu_run")
+(define-key *root-map* (kbd "quoteleft") "urxvt")
+(define-key *root-map* (kbd "s-quoteleft") "urxvt")
+(define-key *top-map* (kbd "SunPrint_Screen") "capture-screen")
+(define-key *top-map* (kbd "M-SunPrint_Screen") "capture-window")
+
+  
 ;; (define-key *top-map* (stumpwm:kbd "s-Left") "move-focus left")
 ;; (define-key *top-map* (stumpwm:kbd "s-j") "move-focus down")
 ;; (define-key *top-map* (stumpwm:kbd "s-k") "move-focus up")
@@ -99,6 +137,8 @@
 ;;(define-key *root-map* (kbd "C-s") "colon1 exec x-terminal-emulator -e ssh ")
 ;; Lock screen
 ;;(define-key *root-map* (kbd "C-l") "exec xlock")
+
+
 
 ;; Web jump (works for Google and Imdb)
 (defmacro make-web-jump (name prefix)
@@ -120,7 +160,13 @@
 (setf *message-window-gravity* :center)
 (setf *input-window-gravity* :center)
 
-;;; Define window placement policy...
+;; remove window borders
+(setf *window-border-style* :none)
+;; This setting would cause urxvt to fill the frame.
+;; see https://github.com/stumpwm/stumpwm/issues/843
+(setf *ignore-wm-inc-hints* t)
+
+;;; Define window placement policy
 
 ;; Clear rules
 (clear-window-placement-rules)
@@ -134,29 +180,44 @@
 ;; existing group using *data-dir*/restore file.
 (define-frame-preference "Default"
   ;; frame raise lock (lock AND raise == jumpto)
-  (0 t nil :class "Konqueror" :role "...konqueror-mainwindow")
-  (1 t nil :class "XTerm"))
-
-(define-frame-preference "Ardour"
-  (0 t   t   :instance "ardour_editor" :type :normal)
-  (0 t   t   :title "Ardour - Session Control")
-  (0 nil nil :class "XTerm")
-  (1 t   nil :type :normal)
-  (1 t   t   :instance "ardour_mixer")
-  (2 t   t   :instance "jvmetro")
-  (1 t   t   :instance "qjackctl")
-  (3 t   t   :instance "qjackctl" :role "qjackctlMainForm"))
-
-(define-frame-preference "Shareland"
-  (0 t   nil :class "XTerm")
-  (1 nil t   :class "aMule"))
+  ;;(0 t nil :class "Konqueror" :role "...konqueror-mainwindow")
+;;  (1 t nil :class "XTerm")
+  (0 t t :class "URxvt")
+  (0 t t :class "Far2l"))
 
 (define-frame-preference "Emacs"
-  (1 t t :restore "emacs-editing-dump" :title "...xdvi")
-  (0 t t :create "emacs-dump" :class "Emacs"))
+  (0 t t :class "Emacs"))
 
+(define-frame-preference "Browser"
+  (0 t t :role "browser"))
+
+(define-frame-preference "Chat"
+  (0 t t :class "discord"))
+
+(define-frame-preference "Lisp"
+  (0 t t :class "Lispworks"))
+
+(define-frame-preference "Games"
+  (0 t t :class "uo.bin")
+  (0 t t :class "client.exe"))
+
+
+;; Create groups
+(add-group (current-screen) "Emacs")
+(add-group (current-screen) "Browser")
+(add-group (current-screen) "Chat")
+(add-group (current-screen) "Lisp")
+(add-group (current-screen) "Games")
+(add-group (current-screen) "Misc 1")
+(add-group (current-screen) "Misc 2")
+(add-group (current-screen) "Misc 3")
+(add-group (current-screen) "Misc 4")
 
 ;; create Slynk server
-(ql:quickload :slynk)
+(ql:quickload "slynk")
 (slynk:create-server :port 4006 :dont-close t)
-(message "Slynk server on port 4006 created")
+
+
+;; set keyboard layout
+(run-shell-command "setxkbmap -layout dvorak,ru -option \"grp:alt_shift_toggle,caps:ctrl_modifier\"")
+;; Run this command to restore capslock: (run-shell-command "setxkbmap -option")
